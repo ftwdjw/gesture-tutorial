380c8d18eb5177ca7753a56ca58d46a17aff0902
ftwdjw <ftwdjw@gmail.com>
2016-12-04 12:46:33 -0600

    Initial Commit

---
+++ gesturesTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gesturesTutorial
+//
+//  Created by John Mac on 12/4/16.
+//  Copyright © 2016 John Wetters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
